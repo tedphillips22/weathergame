0794269814b25b4e5c38fe865282eae569a8a32b
Ted Phillips <tedphillips22@gmail.com>
2022-10-26 16:56:56 -0400

    Continued building basic infrastructure

--- build1/app.py
+++ build1/app.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -14,10 +15,22 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("userid") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
@@ -47,7 +60,7 @@ def login():
             return message("invalid username and/or password")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["userid"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
@@ -89,18 +102,51 @@ def register():
 
         return render_template("register.html")
 
+@app.route("/maketeam", methods = ["POST", "GET"])
+@login_required
+def maketeam():
+    if request.method == "POST":
+        
+        teamname = request.form.get("teamname")
+        #Check for team name dupes
+        
+        
+        userid = session["userid"]
+        
+        db.execute("INSERT INTO teams (userid, teamname) VALUES (?, ?)", userid, teamname)
 
+        teamid = db.execute("SELECT id FROM teams WHERE teamname = ? AND userid = ?", teamname, userid)[0]['id']
+        print("made it")
+        city1 = request.form.get("city1")
+        city1id = citycheck(city1)
+        db.execute("INSERT INTO teamcities (teamid, cityid) VALUES (?, ?)", teamid, city1id)
+
+        city2 = request.form.get("city2")
+        city2id = citycheck(city2)
+        db.execute("INSERT INTO teamcities (teamid, cityid) VALUES (?, ?)", teamid, city2id)
+
+        city3 = request.form.get("city3")
+        city3id = citycheck(city3)
+        db.execute("INSERT INTO teamcities (teamid, cityid) VALUES (?, ?)", teamid, city3id)
+
+        return message("Team named {0} created".format(teamname))
+
+    else:
+        cities = db.execute("SELECT city FROM cities")
+        return render_template("maketeam.html", cities = cities)
+        
 
 def message(message):
     """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
 
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("message.html", message=escape(message))
+    return render_template("message.html", message=message)
+
+
+def citycheck(city):
+    if len(city) != 0:
+            cityid = db.execute("SELECT id FROM cities WHERE city = ?", city)[0]['id']
+            if cityid is None:
+                return message("Some cities not found. Use autocomplete.")
+            return(cityid)
+    else:
+        return message("Please input 3 cities")
