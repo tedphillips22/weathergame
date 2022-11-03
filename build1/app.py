b43bc68c5d8ad40c5267d9c9fc16a809cf67d701
Ted Phillips <tedphillips22@gmail.com>
2022-11-03 09:38:04 -0400

    Cleaned up website structure, cleaned up code with functions to generate dicts of team/league info, added navbar links, built basic homepage

--- build1/app.py
+++ build1/app.py
@@ -17,7 +17,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-##### HELPER FUNCTIONS ######
+
+
+############################################################################################
+##### HELPER FUNCTIONS #####################################################################
+############################################################################################
+
 
 def login_required(f):
     """
@@ -59,13 +64,118 @@ def citycheck(city):
     cityid = db.execute("SELECT id FROM cities WHERE city_state = ?", city)[0]['id']
     return(cityid)
 
+def getusersleaguenames(userid): #outputs dict of leaguenames for given userID
+    leagueids = db.execute ("SELECT leagueid FROM leaguemembers WHERE userid = ?", userid)
 
-####### PAGE FUNCTIONS #######
+    #convert league IDS to league names
+    counter = 0
+    leaguenames = []
+    for row in leagueids:
+        leagueid = leagueids[counter]['leagueid']
+        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = ?", leagueid)[0]['leaguename']
+        leaguenames.append({'leaguename' : leaguename})
+        counter += 1
+
+    return(leaguenames)
+
+def getusersteamnames(userid): #outputs dict of teamnames for given userID
+    teamnames = db.execute("SELECT teamname FROM teams WHERE userid = ?", userid)
+    return(teamnames)
+
+def getusersleagueteamdicts(userid): #outputs dict of leaguename/leagueID/teamname for given userID
+    leagueidsdict = db.execute('SELECT leagueid FROM leaguemembers WHERE userid = (?)', userid)
+    leagueids = []
+
+    #generate list of leagueids for user:
+    counter = 0
+    for row in leagueidsdict:
+        temp = leagueidsdict[counter]
+        leagueidtemp = temp["leagueid"]
+        leagueids.append(leagueidtemp)
+        counter += 1
+    
+    #convert ids to leaguenames:
+    usersleagueteamdict = []
+    counter = 0
+    for id in leagueids:
+        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = (?)", leagueids[counter])[0]["leaguename"]
+        teamid = db.execute("SELECT teamid FROM leaguemembers WHERE userid = (?) AND leagueid = (?)", userid, leagueids[counter])[0]['teamid']
+        teamname = db.execute("SELECT teamname FROM teams WHERE id = (?)", teamid)[0]['teamname']
+        usersleagueteamdict.append({'leaguename' : leaguename, 'leagueid' : leagueids[counter], 'teamname': teamname})
+        counter += 1
+    
+    return(usersleagueteamdict)
+
+def getleagueinfo(leagueid): #outputs dict with teamname/username pairs for given leagueID
+    teamsdict = db.execute("SELECT teamid FROM leaguemembers WHERE leagueid = (?)", leagueid)
+
+    #loop through dict of team IDS and generate list of teams & owners
+    counter = 0
+    leaguelist = []
+    for row in teamsdict:
+        teamname = (db.execute("SELECT teamname FROM teams WHERE id = (?)", teamsdict[counter]["teamid"])[0]['teamname'])
+        useridtemp = db.execute("SELECT userid FROM teams WHERE id = (?)", teamsdict[counter]["teamid"])[0]['userid']
+        username = db.execute("SELECT username FROM users WHERE id = (?)", useridtemp)[0]['username']
+
+        leaguelist.append({'teamname' : teamname, 'username' : username})
+    
+    return(leaguelist)
+
+def getteamcitiesweather(teamid): #outputs dict with city_state names and current weather info for those cities
+    citiesdict = db.execute("SELECT cityid FROM teamcities WHERE teamid = (?)", teamid)
+
+    #loop through dict of city IDS and generate list of cities
+    counter = 0
+    citylist = []
+    for row in citiesdict:
+        cityid = citiesdict[counter]['cityid']
+        cityname = db.execute("SELECT city_state FROM cities WHERE id = (?)", cityid)[0]['city_state']
+        
+        # query API for weather info and add to citylist
+        weathertemp = get_current_weather(cityid)
+
+        temperature = weathertemp['hourly']['temperature_2m'][0]
+        feelslike = weathertemp['hourly']['apparent_temperature'][0]
+        weathercode = weathertemp['hourly']['weathercode'][0]
+        weatherdesc = db.execute("SELECT description FROM weathercodes WHERE code = ?", weathercode)[0]['description']
+
+        citylist.append({'cityname' : cityname, 'temperature' : temperature, 'feelslike' : feelslike, 'weatherdesc' : weatherdesc})
+        counter += 1
+    
+    return(citylist)
+
+def getteamsleagueinfo(teamid): #outputs dict of leaguenames for a given teamid
+    leagueids = db.execute("SELECT leagueid FROM leaguemembers WHERE teamid = ?", teamid)
+    counter = 0
+    leaguenames = []
+    for row in leagueids:
+        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = ?", leagueids[counter]['leagueid'])[0]['leaguename']
+        leaguenames.append({'leaguename' : leaguename})
+    
+    return(leaguenames)
+
+
+
+
+
+########################################################################################
+####### PAGE FUNCTIONS #################################################################
+########################################################################################
+
+
+
+
 
 @app.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    userid = session["userid"]
+
+    leaguedict = getusersleagueteamdicts(userid)
+
+    teamnames = getusersteamnames(userid)
+
+    return render_template("index.html", leaguedict = leaguedict, teamnames = teamnames)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -141,7 +251,9 @@ def maketeam():
     if request.method == "POST":
         
         teamname = request.form.get("teamname")
-        #Check for team name dupes
+        
+        if db.execute("SELECT count(*) FROM teams WHERE teamname LIKE ?", teamname) != 0:
+            return message("Team name taken. Please try again.", 1)
         
         userid = session["userid"]
         
@@ -257,27 +369,7 @@ def makeleague():
 def myleagues():
     userid = session["userid"]
 
-    leagueidsdict = db.execute('SELECT leagueid FROM leaguemembers WHERE userid = (?)', userid)
-    leagueids = []
-
-    #generate list of leagueids for user:
-    counter = 0
-    for row in leagueidsdict:
-        temp = leagueidsdict[counter]
-        leagueidtemp = temp["leagueid"]
-        leagueids.append(leagueidtemp)
-        counter += 1
-    
-    #convert ids to leaguenames:
-    leaguedict = []
-    counter = 0
-    for id in leagueids:
-        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = (?)", leagueids[counter])[0]["leaguename"]
-        teamid = db.execute("SELECT teamid FROM leaguemembers WHERE userid = (?) AND leagueid = (?)", userid, leagueids[counter])[0]['teamid']
-        teamname = db.execute("SELECT teamname FROM teams WHERE id = (?)", teamid)[0]['teamname']
-        leaguedict.append({'leaguename' : leaguename, 'leagueid' : leagueids[counter], 'teamname': teamname})
-        counter += 1
-
+    leaguedict = getusersleagueteamdicts(userid)
 
     return render_template("myleagues.html", leaguedict = leaguedict)
 
@@ -336,17 +428,7 @@ def leaguepage(leaguename):
 
     lleaguename = db.execute("SELECT leaguename FROM leagues WHERE id = (?)", leagueid)[0]['leaguename']
 
-    teamsdict = db.execute("SELECT teamid FROM leaguemembers WHERE leagueid = (?)", leagueid)
-
-    #loop through dict of team IDS and generate list of teams & owners
-    counter = 0
-    leaguelist = []
-    for row in teamsdict:
-        teamname = (db.execute("SELECT teamname FROM teams WHERE id = (?)", teamsdict[counter]["teamid"])[0]['teamname'])
-        useridtemp = db.execute("SELECT userid FROM teams WHERE id = (?)", teamsdict[counter]["teamid"])[0]['userid']
-        username = db.execute("SELECT username FROM users WHERE id = (?)", useridtemp)[0]['username']
-
-        leaguelist.append({'teamname' : teamname, 'username' : username})
+    leaguelist = getleagueinfo(leagueid)
 
     return render_template("league.html", lleaguename = lleaguename, leaguelist = leaguelist)
 
@@ -358,32 +440,9 @@ def teampage(teamname):
 
     tteamname = db.execute("SELECT teamname FROM teams WHERE id = (?)", teamid)[0]['teamname']
 
-    citiesdict = db.execute("SELECT cityid FROM teamcities WHERE teamid = (?)", teamid)
+    citylist = getteamcitiesweather(teamid)
 
-    #loop through dict of city IDS and generate list of cities
-    counter = 0
-    citylist = []
-    for row in citiesdict:
-        cityid = citiesdict[counter]['cityid']
-        cityname = db.execute("SELECT city FROM cities WHERE id = (?)", cityid)[0]['city']
-        
-        # query API for weather info and add to citylist
-        weathertemp = get_current_weather(cityid)
-
-        temperature = weathertemp['hourly']['temperature_2m'][0]
-        feelslike = weathertemp['hourly']['apparent_temperature'][0]
-        weathercode = weathertemp['hourly']['weathercode'][0]
-        weatherdesc = db.execute("SELECT description FROM weathercodes WHERE code = ?", weathercode)[0]['description']
-
-        citylist.append({'cityname' : cityname, 'temperature' : temperature, 'feelslike' : feelslike, 'weatherdesc' : weatherdesc})
-        counter += 1
-
-    leagueids = db.execute("SELECT leagueid FROM leaguemembers WHERE teamid = ?", teamid)
-    counter = 0
-    leaguenames = []
-    for row in leagueids:
-        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = ?", leagueids[counter]['leagueid'])[0]['leaguename']
-        leaguenames.append({'leaguename' : leaguename})
+    leaguenames = getteamsleagueinfo(teamid)
 
     return render_template("team.html", tteamname = tteamname, citylist = citylist, leaguenames = leaguenames)
 
@@ -397,17 +456,8 @@ def userpage(username):
 
     uusername = db.execute("SELECT username FROM users WHERE id = ?", pageid)[0]['username']
 
-    teamsdict = db.execute("SELECT teamname FROM teams WHERE userid = ?", pageid)
+    teamnames = getusersteamnames(pageid)
 
-    leagueids = db.execute ("SELECT leagueid FROM leaguemembers WHERE userid = ?", pageid)
-
-    #convert league IDS to league names
-    counter = 0
-    leaguenames = []
-    for row in leagueids:
-        leagueid = leagueids[counter]['leagueid']
-        leaguename = db.execute("SELECT leaguename FROM leagues WHERE id = ?", leagueid)[0]['leaguename']
-        leaguenames.append({'leaguename' : leaguename})
-        counter += 1
+    leaguenames = getusersleaguenames(pageid)
     
-    return render_template("user.html", uusername = uusername, teamsdict = teamsdict, leaguenames = leaguenames)
+    return render_template("user.html", uusername = uusername, teamnames = teamnames, leaguenames = leaguenames)
